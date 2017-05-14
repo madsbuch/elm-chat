956d6756550ced00f6f8b42a0cdbe4b3cf093316
Mads Buch <madspbuch@gmail.com>
2017-05-14 12:00:05 +0200

    for gh-pages

--- src/Config.elm
+++
@@ -1,4 +0,0 @@
-module Config exposing (..)
-
---socketAddr = "ws://localhost:9160"
-socketAddr = "ws://echo.websocket.org"
--- src/Subscriptions.elm
+++
@@ -1,9 +0,0 @@
-module Subscriptions exposing (..)
-
-import WebSocket
-import Msg exposing (..)
-import Model exposing (..)
-import Config exposing (socketAddr)
-
-subscriptions : Model -> Sub Msg
-subscriptions model = WebSocket.listen socketAddr NewMessage
