4041f32f62fce73383820b835f323a60fb9b3438
Mads Buch <madspbuch@gmail.com>
2017-05-21 19:53:27 +0200

    refactored layout specifics to the layout file

--- src/Layout.elm
+++ src/Layout.elm
@@ -1,15 +1,34 @@
-module Layout exposing (..)
+module Layout exposing (layout)
 
 import Html exposing (..)
 import Material.Layout as Layout
 import Material.Scheme
-import Msg exposing (..)
+import Material
+import Material.Grid as G
 
-makePage main model =
+import Msg exposing (..)
+import Model exposing (..)
+
+import View.InputForm exposing (..)
+import View.ListMessages exposing (..)
+
+layout model =
     Layout.render Mdl model.mdl
       [ Layout.fixedHeader ]
       { header = [ Layout.title [] [(Layout.row [] [h3 [] [text model.title]])]]
       , drawer = []
       , tabs = ([], [])
-      , main = main
+      , main = (top model)
       } |> Material.Scheme.top
+
+top : Model -> List (Html Msg)
+top model =
+  [G.grid []
+       [ G.cell [ G.size G.Tablet 2, G.size G.Desktop 3, G.size G.Phone 0 ]
+           []
+       , G.cell [ G.size G.Tablet 8, G.size G.Desktop 6, G.size G.Phone 12 ]
+           [ div []
+             (    (inputForm model)
+               ++ [listMessages model])
+           ]
+       ]]
--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,6 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-
-
-import Material
-import Material.Grid as G
 
 import Model exposing (..)
 import Msg exposing (..)
@@ -14,33 +8,11 @@ import Update exposing (..)
 import Subscriptions exposing (..)
 import Layout exposing (..)
 
-import View.InputForm exposing (..)
-import View.ListMessages exposing (..)
-
-
--- VIEW
-view : Model -> Html Msg
-view model = makePage (top model) model
-
-top : Model -> List (Html Msg)
-top model =
-  [G.grid []
-       [ G.cell [ G.size G.Tablet 2, G.size G.Desktop 3, G.size G.Phone 0 ]
-           []
-       , G.cell [ G.size G.Tablet 8, G.size G.Desktop 6, G.size G.Phone 12 ]
-           [ div []
-             (    (inputForm model)
-               ++ [listMessages model])
-           ]
-       ]]
-
--- MAIN
-
 main : Program Never Model Msg
 main =
     program
         { init = init
-        , view = view
+        , view = layout
         , update = update
         , subscriptions = subscriptions
         }
